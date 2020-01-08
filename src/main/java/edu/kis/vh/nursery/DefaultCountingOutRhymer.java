cc475f9b9d5a2884507b3a1a96f561df7e3c6833
209476 <209476@edu.p.lodz.pl>
2020-01-08 18:50:08 +0100

    Zmiany 1.3 v2

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,6 +1,6 @@
 package edu.kis.vh.nursery;
 
-public class defaultCountingOutRhymer {
+public class DefaultCountingOutRhymer {
 
     public static final int SIZE = 12;
     public static final int CAPACITY = 11;
